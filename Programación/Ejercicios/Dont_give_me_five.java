ee8ef5297dc406492b5a44cb06826b6c58eb6a55
BernatL99 <116670667+BernatL99@users.noreply.github.com>
2024-01-18 23:45:50 +0100

    Update Dont_give_me_five.java

--- Programación/Ejercicios/Dont_give_me_five.java
+++ Programación/Ejercicios/Dont_give_me_five.java
@@ -1,14 +1,10 @@
-public class Kata
-{
-  public static int dontGiveMeFive(int start, int end)
-  {
-    int contador = 0;
-    for(int i = start; i<=end; i++){
-      contador++;
-      if(i%10==5){
-        i++;
-      }
+public class Kata{
+  public static int dontGiveMeFive(int start, int end){
+    //ans es el contador
+    int ans = 0;
+    for (int i = start; i <= end; i++) {
+      if (!("" + i).contains("5")) ans++;
     }
-    return contador;
+    return ans;
   }
 }
