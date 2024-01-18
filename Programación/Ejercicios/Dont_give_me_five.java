4f66ea91eec1243e69d68201393dd5f33aa8d9d6
BernatL99 <116670667+BernatL99@users.noreply.github.com>
2024-01-19 00:12:06 +0100

    Update Dont_give_me_five.java

--- Programación/Ejercicios/Dont_give_me_five.java
+++ Programación/Ejercicios/Dont_give_me_five.java
@@ -3,8 +3,8 @@ public class Kata{
     //ans es el contador
     int ans = 0;
     for (int i = start; i <= end; i++) {
-      if (!("" + i).contains("5")) ans++;
-    }
-    return ans;
+      if (!("" + i).contains("5")) ans++;                      // Usamos ! para definir que tiene que ser lo contrario
+    }                                                          // Hacemos ("" + i) para convertir lo sacado de i en un string
+    return ans;                                                // Añadimos .contains("5") para que encuemtre cualquier numero con un 5.
   }
 }
