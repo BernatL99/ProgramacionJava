0ff19539e2962df481d9411b9c5eee7d3f0fd897
BernatL99 <116670667+BernatL99@users.noreply.github.com>
2024-01-25 11:30:27 +0100

    Update CountTheSmileyFaces.java

--- Programación/Ejercicios/CountTheSmileyFaces.java
+++ Programación/Ejercicios/CountTheSmileyFaces.java
@@ -1,5 +1,5 @@
 import java.util.*;
-\\ Primer intento del ejercicio https://www.codewars.com/kata/583203e6eb35d7980400002a/train/java
+//Resultado final del trabajo https://www.codewars.com/kata/583203e6eb35d7980400002a/train/java
 public class SmileFaces {
   
   public static int countSmileys(List<String> arr) {
@@ -7,11 +7,9 @@ public class SmileFaces {
         return 0;
       }
       int contador = 0;
+      String cara = "[:;]([-~]?[)D])";
       for(int i = 0;i < arr.size(); i++){
-        if((arr.get(i).contains(":)")) || (arr.get(i).contains(":-)")) || (arr.get(i).contains(";)")) ||
-           (arr.get(i).contains(";-)")) || (arr.get(i).contains(":~)")) || (arr.get(i).contains(";~)")) ||
-           (arr.get(i).contains(":D")) || (arr.get(i).contains(":-D")) || (arr.get(i).contains(";D")) ||
-           (arr.get(i).contains(";-D")) || (arr.get(i).contains(":~D")) || (arr.get(i).contains(";~D"))){
+        if(arr.get(i).matches(cara)){
           contador++;
         }  
      }
