18126989a1ee9d4c70627b50316e6c54ff2ea3f6
BernatL99 <116670667+BernatL99@users.noreply.github.com>
2025-11-11 19:57:00 +0100

    Delete Programación directory

--- Programación/Bucles/for/for_2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Bucle_2 {
-	public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-        for(int i = 0;  i <= 100 ; i+=7){
-        System.out.println(i);
-        }
-    }
-}
--- Programación/Ejercicios/MúltiplosDe3o5.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-  public int solution(int number) {
-    int suma = 0;
-    for(int i = number-1; i >= 3 ; i--){
-      if(i % 3 == 0 || i % 5 == 0)suma += i;
-    }
-    return suma;
-  }
-}
